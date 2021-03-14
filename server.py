128394183ae9f285eb55d5aeae179f58772938a6
pomidor <vladimirelizarov89@gmail.com>
2021-03-14 23:43:23 +0300

    add selectors

--- server.py
+++ server.py
@@ -1,9 +1,39 @@
 import socket
 import re
+import selectors
 
 HOST = '127.0.0.1'
 PORT = 65432
 global_data = {}
+sel = selectors.DefaultSelector()
+
+
+def read(conn, mask):
+    data = conn.recv(1024)  # Should be ready
+    if data:
+        action, key, value = split_input_data(data)
+        if action == "get":
+            conn.sendall(get_data(key))
+        elif action == "set":
+            if set_data(key, value):
+                conn.sendall(b"OK")
+            else:
+                conn.sendall(b"Error while setting data")
+        elif action == "error":
+            conn.sendall(value.encode())
+        else:
+            conn.sendall(b"Unhandled error")
+    else:
+        print('closing', conn)
+        sel.unregister(conn)
+        conn.close()
+
+
+def accept(sock, mask):
+    conn, addr = sock.accept()  # Should be ready
+    print('accepted', conn, 'from', addr)
+    conn.setblocking(False)
+    sel.register(conn, selectors.EVENT_READ, read)
 
 
 def set_data(key:str, value:str) -> bool:
@@ -31,7 +61,7 @@ def split_input_data(data: bytes) -> tuple:
     data_decoded = data.decode()
     m = re.match(r"(\w+)\s(\w+)\s*(.*)", data_decoded)
     if m is None:
-        return "error", "value", "Error while split data_decoded"
+        return "error", "value", f"Error while split data_decoded {data_decoded}"
     else:
         return m.group(1, 2, 3)
 
@@ -39,26 +69,15 @@ def split_input_data(data: bytes) -> tuple:
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
+    s.setblocking(False)
     print(f"Listen {HOST}:{PORT}")
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            action, key, value = split_input_data(data)
-            if action == "get":
-                conn.sendall(get_data(key))
-            elif action == "set":
-                if set_data(key, value):
-                    conn.sendall(b"OK")
-                else:
-                    conn.sendall(b"Error while setting data")
-            elif action == "error":
-                conn.sendall(value.encode())
-            else:
-                conn.sendall(b"Unhandled error")
+    sel.register(s, selectors.EVENT_READ, accept)
+
+    while True:
+        events = sel.select()
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj, mask)
 
 
 
